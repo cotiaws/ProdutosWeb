c32e0220f85e65fe8a818a23e390040371ecad91
cotiaws <cotiaws5@gmail.com>
2025-11-13 22:08:15 -0300

    Criação de componentes, navegação em rotas e criação das páginas de cadastro e consulta de produtos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,25 @@
 import { Routes } from '@angular/router';
+import { CadastrarProdutos } from './cadastrar-produtos/cadastrar-produtos';
+import { ConsultarProdutos } from './consultar-produtos/consultar-produtos';
+import { EditarProdutos } from './editar-produtos/editar-produtos';
 
-export const routes: Routes = [];
+//Mapeamento das rotas da aplicação
+export const routes: Routes = [
+    {
+        path: 'cadastrar-produtos', //rota
+        component: CadastrarProdutos //componente
+    },
+    {
+        path: 'consultar-produtos', //rota
+        component: ConsultarProdutos //componente
+    },
+    {
+        path: 'editar-produtos', //rota
+        component: EditarProdutos //componente
+    },
+    {
+        path: '', //rota padrão
+        redirectTo: '/consultar-produtos', //redirecionamento
+        pathMatch: 'full' //corresponde exatamente à rota vazia
+    }
+];
